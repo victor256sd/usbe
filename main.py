e5e2575fb45ee2bded46b28a7e34e59b36dcd060
victor256sd <victor256@gmail.com>
2026-06-05 21:40:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ if st.session_state.get('authentication_status'):
         f"""
         <img src="https://raw.githubusercontent.com/victor256sd/thecode/main/image.png" alt="edmse" width=700 usemap="#workmap">
         <map name="workmap">
-            <area shape="rect" coords="420,14,691,94" alt="nasdtec" href="www.nasdtec.net">
+            <area shape="rect" coords="420,14,691,94" alt="nasdtec" href="http://www.nasdtec.net">
             <area shape="circle" coords="196,171,66" alt="ncaee" href="https://www.educatorethics.org">
         </map>        
         """,
